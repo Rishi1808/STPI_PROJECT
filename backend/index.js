fa9f9e1c73efab400bc4ecec7452b6073036ae49
Ayush kumar <157474501+Ayushkumar1505@users.noreply.github.com>
2025-04-10 15:44:53 +0530

    updated index,js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const cors = require("cors");
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// hello world 
 
 const PORT = process.env.PORT || 5000;
 
