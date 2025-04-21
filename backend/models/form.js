73cfaa1350291e80c0bd7a1fcf1a6cc5c4b22b43
Rishi Raj <106508188+Rishi1808@users.noreply.github.com>
2025-04-21 12:00:28 +0530

    db check

--- backend/models/form.js
+++ backend/models/form.js
@@ -175,15 +175,9 @@ const formSchema = new mongoose.Schema({
   // Application Status
   applicationStatus: {
     type: String,
-    enum: ['pending', 'approved', 'rejected'],
-    default: 'pending'
+    enum: ['Pending', 'Accepted', 'Rejected'],
   },
-  status: {
-    type: String,
-    enum: ['Pending', 'Accepted', 'Rejected'], // Ensure 'Accepted' is listed here
-    default: 'Pending',
-  },
-  
+
   // Admin comments
   adminComments: String,
   
@@ -233,49 +227,3 @@ module.exports = Form;
 
 
 
-// const mongoose = require("mongoose");
-
-// const formSchema = new mongoose.Schema({
-//     dateOfVisit: Date,
-//     bankBranchAdd: String,
-//     bankName: String,
-//     AccountNo: String,
-//     boardOfDirectors: String,
-//     email: String,
-//     gst: String,
-//     pan: String,
-//     contactPerson: String,
-//     unitName: String,
-//     authLetterFile: Boolean,
-//     registeredAddress: String,
-//     status: String,
-//     sc_st: Boolean,
-//     women: Boolean,
-//     divyangjan: Boolean,
-//     involvement: String,
-//     phone: String,
-//     purposeOfVisit: String,
-//     from: String,
-//     to: String,
-//     incubation_space: String,
-//     power_backup: String,
-//     internet_requirement: String,
-//     fax: Boolean,
-//     photocopy: Boolean,
-//     phone_call: Boolean,
-//     conference: Boolean,
-//     lease_rent: String,
-//     electricity_charges: String,
-//     power_backup_charges: String,
-//     business_support_facilities: String,
-//     dd_no: String,
-//     dd_date: Date,
-//     deposit_amount: String,
-//     expected_occupancy_date: Date,
-//     months: String,
-//     uploadedFiles: [String]
-// });
-
-// const Form = mongoose.model("Form", formSchema);
-
-// module.exports = Form;
